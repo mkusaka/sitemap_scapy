2ec945afb022feb14bf4f9f4079fb0568f5e3a5c
mkusaka <hinoshita1992@gmail.com>
2019-03-07 23:12:00 +0900

    wired

--- sitemapper/sitemapper/spiders/sitemap.py
+++ sitemapper/sitemapper/spiders/sitemap.py
@@ -4,12 +4,18 @@ import scrapy
 
 class SitemapSpider(scrapy.Spider):
     name = 'sitemap'
-    allowed_domains = ['example.com']
+    # allowed_domains = ['example.com']
     # start_urls = ['http://example.com/']
 
-    sitemap_urls = ['https://sutaba-mac.site/robots.txt']
+    # sitemap_urls = ['https://wired.jp/sitemap.xml']
+
+    # sitemap_follow = ['2018-10']
+
+    # sitemap_rules = [(r'/2018/10/', 'parse')]
+
+    # https://imabari.hateblo.jp/entry/2018/12/01/155452
+
     def parse(self, response):
-        # URLとタイトルの辞書(dictionary)を返す
         yield {
             'url': response.url,
             'title': response.css('title::text').extract_first(),
