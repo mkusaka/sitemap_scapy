356d0e4853818a834446aa724b56a01a61f5a0b0
mkusaka <hinoshita1992@gmail.com>
2019-05-14 19:52:43 +0900

    use sitemap spider

--- sitemapper/sitemapper/spiders/sitemap.py
+++ sitemapper/sitemapper/spiders/sitemap.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.spiders import SitemapSpider as ParentSitemapSpider
 
-
-class SitemapSpider(scrapy.Spider):
+class SitemapSpider(ParentSitemapSpider):
     name = 'sitemap'
     allowed_domains = ['example.com']
     # start_urls = ['http://example.com/']
 
-    sitemap_urls = ['https://sutaba-mac.site/robots.txt']
+    sitemap_urls = ['https://wired.jp/sitemap.xml']
+
+    sitemap_rules = [(r'/2018/10/', 'parse')]
     def parse(self, response):
-        # URLとタイトルの辞書(dictionary)を返す
-        yield {
-            'url': response.url,
-            'title': response.css('title::text').extract_first(),
-        }
+        i = {}
+        i['title'] = response.css(
+            'article.article-detail > header > h1::text').extract_first()
+        return i
+    # ref https://imabari.hateblo.jp/entry/2018/12/01/155452
