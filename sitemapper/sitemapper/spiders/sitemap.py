4240fd01b98a109cb1783cab700179381383a680
mkusaka <hinoshita1992@gmail.com>
2019-05-14 21:25:54 +0900

    wip

--- sitemapper/sitemapper/spiders/sitemap.py
+++ sitemapper/sitemapper/spiders/sitemap.py
@@ -4,16 +4,16 @@ import scrapy
 
 class SitemapSpider(scrapy.Spider):
     name = 'sitemap'
-    # allowed_domains = ['example.com']
-    # start_urls = ['http://example.com/']
-
-    # sitemap_urls = ['https://wired.jp/sitemap.xml']
-
-    # sitemap_follow = ['2018-10']
-
-    # sitemap_rules = [(r'/2018/10/', 'parse')]
-
-    # https://imabari.hateblo.jp/entry/2018/12/01/155452
+    allowed_domains = ['www.bbc.co.uk']
+    sitemap_urls = [
+        # ここにはrobots.txtのURLを指定してもよいが、
+        # 無関係なサイトマップが多くあるので、今回はサイトマップのURLを直接指定する。
+        'http://www.bbc.co.uk/news/sitemap.xml',
+    ]
+    sitemap_rules = [
+        # 正規表現 '/news/' にマッチするページをparse_newsメソッドでパースする
+        (r'/news/', 'parse'),
+    ]
 
     def parse(self, response):
         yield {
